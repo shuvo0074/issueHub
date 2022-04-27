e7fef57e8a01aa17d74c8bb66a30261423e6172a
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-04-27 23:35:07 +0600

    add search and results screens

---
+++ src/route.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import Search from './screens/Search';
+import Result from './screens/Result';
+
+const Stack = createStackNavigator();
+
+const AppStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Search"
+      component={Search}
+      options={{ title: '', headerShown: false }}
+    />
+    <Stack.Screen
+      name="Result"
+      component={Result}
+      options={{ title: '', headerShown: false }}
+    />
+  </Stack.Navigator>
+);
+
+export default AppStack;
---
+++ src/screens/Search.js
@@ -0,0 +1,111 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useEffect} from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import FetchService from '../services/fetchService';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const Search: () => Node = ({navigation}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    console.log(navigation, '=-==-');
+    FetchService(
+      'GET',
+      'repos/doublesymmetry/react-native-track-player/issues',
+    ).then(res => console.log(res));
+  }, []);
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Result')}
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <View
+            style={{
+              backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            }}>
+            <Section title="Search">
+              Edit <Text style={styles.highlight}>Search</Text>
+            </Section>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default Search;
