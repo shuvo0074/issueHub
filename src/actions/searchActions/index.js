a5c3fa2040ce053177118696c2ff078d0263ea27
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-04-29 15:06:02 +0600

    define search actions

---
+++ src/actions/searchActions/index.js
@@ -0,0 +1,49 @@
+export const syncResultFromAPI = result => {
+  return {
+    type: 'SYNC_RESULT',
+    payload: result,
+  };
+};
+
+export const clearResult = () => {
+  return {
+    type: 'CLEAR_RESULT',
+  };
+};
+
+export const setissueCount = count => {
+  return {
+    type: 'SET_OPEN_ISSUE_COUNT',
+    payload: count,
+  };
+};
+
+export const setUserameSearchParam = username => {
+  return {
+    type: 'SET_USERNAME_PARAM',
+    payload: username,
+  };
+};
+
+export const setRepositoryNameSearchParam = repositoryTitle => {
+  return {
+    type: 'SET_REPOSITORY_PARAM',
+    payload: repositoryTitle,
+  };
+};
+export const toggleSearchState = () => {
+  return {
+    type: 'TOGGLE_CURRENT_STATE',
+  };
+};
+export const setSearchPageIndex = index => {
+  return {
+    type: 'SET_PAGE_INDEX',
+    payload: index,
+  };
+};
+export const toggleResultListEnd = () => {
+  return {
+    type: 'TOGGLE_RESULT_LIST_ENDED',
+  };
+};
