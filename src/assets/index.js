572299669567b7358fc43f818d66994418aa5941
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-04-29 15:39:53 +0600

    add assets

---
+++ src/assets/index.js
@@ -0,0 +1,24 @@
+const global = {
+  logo: require('./global/logo.png'),
+};
+const result = {
+  closed: require('./result/closed.png'),
+  closediconForList: require('./result/closediconForList.png'),
+  comments: require('./result/comments.png'),
+  icNextBlue: require('./result/icNextBlue.png'),
+  icNextGrey: require('./result/icNextGrey.png'),
+  icPreviousBlue: require('./result/icPreviousBlue.png'),
+  icPreviousGrey: require('./result/icPreviousGrey.png'),
+  open: require('./result/open.png'),
+  openIconForList: require('./result/openIconForList.png'),
+};
+const search = {
+  icCorrect: require('./search/icCorrect.png'),
+  searchInputBg: require('./search/searchInputBg.png'),
+};
+
+export const assets = {
+  global,
+  search,
+  result,
+};
