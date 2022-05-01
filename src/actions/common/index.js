d263c13b1563d052cadbbf10290117d79d4784e6
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-05-01 17:59:57 +0600

    handle next button enable/disable from fetchIssues action

--- src/actions/common/index.js
+++ src/actions/common/index.js
@@ -1,16 +1,27 @@
 import FetchService from '../../services/fetchService';
-import {setissueCount, syncResultFromAPI} from '../searchActions';
+import {
+  setissueCount,
+  syncResultFromAPI,
+  toggleResultListEnd,
+} from '../searchActions';
 
 export const fetchIssues = () => (dispatch, getState) => {
+  const {
+    username_param,
+    repository_param,
+    currentState,
+    currentPageIndex,
+    isLastResultPage,
+  } = getState().issues;
   FetchService(
     'GET',
-    `repos/${getState().issues.username_param}/${
-      getState().issues.repository_param
-    }/issues?state=${getState().issues.currentState}&page=${
-      getState().issues.currentPageIndex
-    }`,
+    `repos/${username_param}/${repository_param}/issues?state=${currentState}&page=${currentPageIndex}`,
   ).then(res => {
-    dispatch(syncResultFromAPI(res));
+    if (res.length > 0) {
+      if (isLastResultPage) dispatch(toggleResultListEnd()); // response may have more results, enable next button
+      dispatch(syncResultFromAPI(res)); // if response has results, sync
+      if (res.length < 30) dispatch(toggleResultListEnd()); // issuesPerpage is 30, so if result has less than 30, then disable next button
+    } else dispatch(toggleResultListEnd()); // if response has no results, disable next button
   });
 };
 
