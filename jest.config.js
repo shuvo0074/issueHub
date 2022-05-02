c32d4f5d0fede3bdab5080b09211b87f9a454b00
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-05-02 22:17:01 +0600

    update jest config for gesture-handler

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  "setupFiles": [
+    "./node_modules/react-native-gesture-handler/jestSetup.js"
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
