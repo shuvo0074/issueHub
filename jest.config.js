4f349623cb1f6c3deb4cbc5824d22bdbcd0f7b5a
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-05-02 23:03:20 +0600

    add mock files and jet config for rn gesture handler

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   preset: 'react-native',
-  "setupFiles": [
-    "./node_modules/react-native-gesture-handler/jestSetup.js"
+  setupFiles: [
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+    '<rootDir>/jest/setup.js',
   ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+  },
 };
